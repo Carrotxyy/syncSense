1face65faaeef89db35c3ca946e803c895d501c6
Carrotxyy <1339691266@qq.com>
2020-08-05 09:45:20 +0800

    完善机构同步

--- init.go
+++ init.go
@@ -1,4 +1,4 @@
-package main
+package syncSense
 
 import (
 	"fmt"
@@ -13,19 +13,32 @@ import (
 type Init struct {
 	Work *work.Work `inject:""`
 }
+//
+//func main() {
+//	wxConfig := setting.Config{}
+//	wxConfig.WxAddr = "http://xyz.szlimaiyun.cn"
+//	wxConfig.DbType = "mysql"
+//	wxConfig.DbUser = "root"
+//	wxConfig.DbPassword = "123456"
+//	wxConfig.DbIP = "127.0.0.1"
+//	wxConfig.DbName = "gin-vue"
+//	wxConfig.TablePrefix = "go_"
+//
+//
+//	init,_ := Create(wxConfig)
+//
+//	// 同步Org_SenseMark = "1" 的新增数据
+//	init.Work.AddOrginfoUpload()
+//	// 同步Org_SenseMark = "2" 的修改数据
+//	init.Work.UpdateOrginfoUpload()
+//	// 同步Org_SenseMark = "2" 且 Org_SenseID = "" 特殊情况
+//	init.Work.OtherOrginfoUpload()
+//	// 同步Org_SenseMark = "3" 的删除数据
+//	init.Work.DeleteOrginfoUpload()
+//}
+//
 
-func main() {
-	wxConfig := setting.Config{}
-	wxConfig.WxAddr = "http://xyz.szlimaiyun.cn"
-	wxConfig.DbType = "mysql"
-	wxConfig.DbUser = "root"
-	wxConfig.DbPassword = "123456"
-	wxConfig.DbIP = "127.0.0.1"
-	wxConfig.DbName = "gin-vue"
-	wxConfig.TablePrefix = "go_"
-
-
-	init,_ := Create(wxConfig)
+func (init *Init)RunWork(){
 
 	// 同步Org_SenseMark = "1" 的新增数据
 	init.Work.AddOrginfoUpload()
@@ -37,6 +50,10 @@ func main() {
 	init.Work.DeleteOrginfoUpload()
 }
 
+// 获取配置对象
+func GetConfig()setting.Config{
+	return setting.Config{}
+}
 
 func Create(config setting.Config)(*Init,error){
 
